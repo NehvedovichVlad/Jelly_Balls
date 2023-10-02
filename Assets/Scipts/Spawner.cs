d3f513d9a507f92d1a0dce8f96a1f6dbdce5b4ab
Vladislav <61420773+NehvedovichVlad@users.noreply.github.com>
2023-10-02 06:10:06 -0700

    Add script MoveSpawner

---
+++ Assets/Scipts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private float _sencentivity = 25f;
+    [Tooltip("Максимальное расстояние на которое может смещаться Spawner")]
+    [SerializeField] private float _maxXPosition = 2.5f;
+
+    private float _xPoisition;
+    private float _oldMouseX;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            _oldMouseX = Input.mousePosition.x;
+
+        if (Input.GetMouseButton(0))
+            MoveSpawner();
+    }
+
+    private void MoveSpawner()
+    {
+        float delta = Input.mousePosition.x - _oldMouseX;
+        _oldMouseX = Input.mousePosition.x;
+        _xPoisition += delta * _sencentivity / Screen.width;
+        _xPoisition = Mathf.Clamp(_xPoisition, -_maxXPosition, _maxXPosition);
+        transform.position = new Vector3(_xPoisition, transform.position.y, transform.position.z);
+    }
+}
